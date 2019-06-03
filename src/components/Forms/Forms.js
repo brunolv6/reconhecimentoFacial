fa470681898e385a417216359339e79142c7e5b7
Bruno Valle <brunolv6@gmail.com>
2019-06-03 11:33:40 -0300

    Change whats was wrote

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -4,7 +4,7 @@ import './Style.css';
 const Forms = ({onInputChange, onButtonSubmit}) =>{
     return(
         <div className='tc'>
-            <p><h2>Let's try the Magic Brain! </h2></p>
+            <p><h2>Let's try the Magic Brain! Find a FACE in your photo! </h2></p>
             <div className='centro'>
                 <div className='amarelo shadow-5'>
                     <div className='centro'>
