f21fb3e3586fa580de333d7dc02bf9b50f6e9c31
Bruno Valle <brunolv6@gmail.com>
2019-06-04 12:27:15 -0300

    Correction of signIn

--- src/components/SignForm/SignForm.js
+++ src/components/SignForm/SignForm.js
@@ -19,7 +19,28 @@ class SignForm extends Component {
            .then(data => console.log(data)) //podia ser só ".then(console.log())"
    }
 
+   onEmailChange = (event) => {
+       this.setState({email: event.target.value});
+   }
+
+   onPasswordChange = (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    onSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method: 'post', //pode ser get, put ou delete tbm
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        });
+        this.props.changePage('home');
+    }
+
     render(){
+        //abreviação abaixo apenas
         const { changePage } = this.props;
         return(
             <div>
@@ -30,15 +51,15 @@ class SignForm extends Component {
                             <legend className="f1 fw6 ph0 mh0 tc">Sign In</legend>
                             <div className="mt3 tc">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-70" type="email" name="email-address"  id="email-address"/>
+                                <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-70" type="email" name="email-address"  id="email-address"/>
                             </div>
                             <div className="mv3 tc">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-70" type="password" name="password"  id="password"/>
+                                <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-70" type="password" name="password"  id="password"/>
                             </div>
                             </fieldset>
                             <div className="tc">
-                            <input onClick={()=> changePage('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+                            <input onClick={this.onSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
                             </div>
                             <div className="lh-copy mt3 tc">
                             <a href="#0" className="f6 link dim black db" onClick={()=> changePage('register')} >Register</a>
